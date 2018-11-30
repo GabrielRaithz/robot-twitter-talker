c7c72dfbcf0597c4f033d00f99734cfb6cfc2ea4
Raithz <gabriel.raithz@transpobrasil.com.br>
2018-11-30 13:36:16 -0200

    ajusts

--- chato.js
+++ chato.js
@@ -8,11 +8,8 @@ bot.loadDirectory("./brain").then(loading_done).catch(loading_error);
 
 function loading_done() {
     bot.sortReplies();
-    console.log("IT'S ALIVE!!");
-
     app.get("/askBot/:text", function (request, response) {
         console.log(request.params.text);
-
         return bot.reply("local-user", request.params.text).then(function (reply) {
             let answer = "The bot says: " + reply;
             console.log(answer);
@@ -25,5 +22,9 @@ function loading_error(error, filename, lineno) {
     console.log("Error when loading files: " + error);
 }
 
-app.listen(8080);
-
+try{
+	app.listen(8080);
+	console.log("IT'S ALIVE!!");
+}catch(err){
+	err.toString()
+}
